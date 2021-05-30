83ec12ad6e65f91cac64604549d3364eb93a7d61
SzymanskiJ <jakub.szymanski@zf.com>
2021-05-30 15:01:41 +0200

    add __str__() method for Node class

--- bin-tree-view.py
+++ bin-tree-view.py
@@ -1,14 +1,20 @@
 
 class Node:
 
-    def __init__(self, v=0, l=None, r=None):
-        self.val = v
-        self.left = l
-        self.right = r
+    def __init__(self, value=0, left_node=None, right_node=None):
+        self.val = value
+        self.left = left_node
+        self.right = right_node
 
-    def print(self):
-        print(f'Node id: { hex(id(self)) }, value: {self.val}')
+    def __str__(self):
+        return f'Node id: {repr(self)}, value: {self.val}, left: {repr(self.left)}, right: {repr(self.right)}';
+
 
 if __name__ == "__main__":
-    n1 = Node(1)
-    n1.print()
+    n2 = Node(2)
+    print(n2)
+    n3 = Node(3)
+    print(n3)
+    n1 = Node(1, Node(4), Node(5))
+    print(n1)
+    print(n1.left)
